a5b2b77d4363de3f9d002874082600ee074af96b
justinjoram <justinjoram@icloud.com>
2025-10-04 17:08:17 +0300

    Add Docker support and enhance user service functionality
    - Introduced Docker setup with `Dockerfile`, `docker-compose`, and `.dockerignore`.
    - Included `build-test-deploy-workflow` and `Qodana` workflows for CI/CD and code quality checks.
    - Updated `UsersService` with in-memory user data and enhanced CRUD operations.
    - Added API global prefix and updated `README.Docker.md` with deployment instructions.

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -4,23 +4,55 @@ import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@example.com',
+      phone: '255742618178'
+    },
+    {
+      id: 2,
+      name: 'Jane',
+      email: 'jane@example.com',
+      phone: '255742618179'
+    },
+    {
+      id: 3,
+      name: 'Alice',
+      email: 'alice@example.com',
+      phone: '255742618180'
+    }
+  ]
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const user = this.users.find(user => user.id === id);
+    if (!user) return 'User not found';
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    const user = this.users.find(user => user.id === id);
+    if (!user) {
+      return 'User not found';
+    }
     return `This action updates a #${id} user`;
   }
 
   remove(id: number) {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) {
+      return 'User not found';
+    }
+    this.users.splice(userIndex, 1);
     return `This action removes a #${id} user`;
   }
 }
